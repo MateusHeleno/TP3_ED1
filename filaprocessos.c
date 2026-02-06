bf80bc91c334cfc56552aab1fa7636869c6d14bb
Mateus Heleno <MATEUSHELENO378@GMAIL.COM>
2026-02-06 13:13:18 -0300

    Inserção das funções base, de alocacao, e refinamento das structs

--- filaprocessos.c
+++ filaprocessos.c
@@ -7,22 +7,106 @@
 
 
 Pilha *criarPilha(){
+    Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+    
+    if(!p)
+        return NULL;
+    
+    p->topo = NULL;
+    p->tam = 0;
+
+    return p;
+}
+
+Pilha *destroiPilha(Pilha *p){
+    if(!p)
+        return NULL;
+    
+    while(p->topo != NULL){
+        desempilha(p);
+    }
+
+    free(p);
     return NULL;
 }
 
-Pilha *destroiPilha(){
-    return NULL;
+void empilha(Pilha *p,Escopo *e){
+    if(!p)
+        return;
+    if(!e)
+        return;
+
+    e->prox = p->topo->prox;
+    
+    p->topo = e;
+
+    p->tam++;
 }
 
-void empilha(){
+void desempilha(Pilha *p){
+    if(!p)
+        return;
+    if(p->tam == 0)
+        return;
 
-}
+    Escopo *aux = p->topo;
 
-void desempilha(){
+    p->topo = p->topo->prox;
 
+    p->tam--;
+    destroiEscopo(aux);// la dentro ela destroi a arvore;
+    free(aux);
 }
 
 void executar(){
 
 }
+
+Escopo *criarEscopo(){
+    Escopo *e = (Escopo*)malloc(sizeof(Escopo));
+    if(!e)
+        return NULL;
+
+    e->raiz = NULL;
+    e->prox = NULL;
+    e->tamAr = 0;
+
+    return e;
+}
+
+Escopo *destroiEscopo(Escopo *e){
+    if(!e)
+        return NULL;
+
+    destroiAr(e->raiz);
+
+    e->raiz = NULL;
+    e->tamAr=0;
+
+    free(e);
+    return NULL;
+
+}
+
+void adicionaVar(){
+
+}
+
+No *criaNo(){
+    return NULL;
+}
+
+void destroiAr(No *no){
+    if(!no)
+        return;
+
+    destroiAr(no->esq);
+    destroiAr(no->dir);
+
+    free(no);
+}
+
+
+
+
 
--- filaprocessos.h
+++ filaprocessos.h
@@ -5,29 +5,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-// structs
-typedef struct {
-    char var[16];
-    int valor;
-}Escopo;
+#define TAM_PALA 16
 
-typedef struct {
-    int x;
+// structs
+
+typedef struct No { // no da arvore
+    char var[TAM_PALA];
+    char valor[TAM_PALA];
+    struct No *esq;
+    struct No *dir;
+} No;
+
+typedef struct Escopo { // termos da pilha
+    No *raiz;
+    struct Escopo *prox; // vai mandandos pros debaixo da pilhar
+    int tamAr;
+} Escopo;
+
+typedef struct { // estrutura gerak da oukha
+    Escopo* topo;
+    int tam;
 }Pilha;
 
 
 // TAD PILHA
 
 Pilha *criarPilha();
-Pilha *destroiPilha();
-void empilha();
-void desempilha();
+Pilha *destroiPilha(Pilha *p);
+void empilha(Pilha *p,Escopo *e);
+void desempilha(Pilha *p);
 void executar();
 
 // TAD ESCOPO
 Escopo *criarEscopo();
-Escopo *destroiEscopo();
+Escopo *destroiEscopo(Escopo *e);
 void adicionaVar();
+// buscarvar();
+
+// TAD NO 
+No *criaNo();
+void destroiAr(No *no);
 
 #endif
 
